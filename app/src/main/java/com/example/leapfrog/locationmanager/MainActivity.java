9590fb2ed47e4ba0fe5bf00a1fa0116055ec58d9
leapfrog <minusarraf96@gmail.com>
2017-12-05 16:14:02 +0545

    Code format

--- app/src/main/java/com/example/leapfrog/locationmanager/MainActivity.java
+++ app/src/main/java/com/example/leapfrog/locationmanager/MainActivity.java
@@ -1,25 +1,12 @@
 package com.example.leapfrog.locationmanager;
 
-import android.Manifest;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
