224444bcbadfc4d13ffb10ea6bb80006e7588a6e
leapfrog <minusarraf96@gmail.com>
2017-12-05 16:10:22 +0545

    Code format

--- app/src/main/java/com/example/leapfrog/locationmanager/MainActivity.java
+++ app/src/main/java/com/example/leapfrog/locationmanager/MainActivity.java
@@ -31,22 +31,19 @@ public class MainActivity extends AppCompatActivity implements LocInterface, Vie
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         btn.setOnClickListener(this);
-
     }
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case Constants.LocationCode : {
-                // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                   // callForLocation();
+                    GetLocation l = new GetLocation(MainActivity.this);
+                    l.callForLocation();
                 } else {
                 }
                 return;
@@ -54,32 +51,19 @@ public class MainActivity extends AppCompatActivity implements LocInterface, Vie
         }
     }
 
-
     @Override
-
     protected void onResume() {
         super.onResume();
-        //Now lets connect to the API
-       // mGoogleApiClient.connect();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.v(this.getClass().getSimpleName(), "onPause()");
-
-       /* //Disconnect from API onPause()
-        if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            mGoogleApiClient.disconnect();
-        }*/
-
-
     }
 
     @Override
     public void location(double lat, double lon) {
-        Toast.makeText(this,"Lat: "+lat+" LOn: " +lon,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Lat: "+lat+" Lon: " +lon,Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -88,4 +72,6 @@ public class MainActivity extends AppCompatActivity implements LocInterface, Vie
         GetLocation l = new GetLocation(MainActivity.this);
         l.callForLocation();
     }
+
 }
+
